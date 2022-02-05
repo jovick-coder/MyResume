ef14c4d92b40a48c0dec27cbdd9180cdcaa4e577
Josiah Victor <victorjosiahm3@gmail.com>
2022-02-05 19:48:09 +0100

    added netlify form submision javascript

--- assets/js/validate.js
+++ assets/js/validate.js
@@ -1,7 +1,33 @@
+let checkArray = []
+// netlify form data encode
+function encode(data) {
+  return Object.keys(data)
+    .map((key) => encodeURIComponent(key) + '=' + encodeURIComponent(data[key]))
+    .join('&')
+}
+// Netlify form submittion
+
+const handleSubmit = (event) => {
+  event.preventDefault()
+  if (checkArray.length !== 4) {
+    console.log('fill all field')
+  } else {
+    fetch('/', {
+      method: 'POST',
+      headers: { 'Content-Type': 'application/x-www-form-urlencoded' },
+      body: encode({
+        'form-name': event.target.getAttribute('name'),
+        ...name,
+      }),
+    })
+      .then(() => navigate('/thank-you/'))
+      .catch((error) => alert(error))
+  }
+}
 const form = document.querySelector('.email-form')
 form.addEventListener('submit', (e) => {
   e.preventDefault()
-
+  checkArray = []
   let inputElementArray = [...e.srcElement]
   let validateElement = [...form.querySelectorAll('.validate')]
 
@@ -10,29 +36,33 @@ form.addEventListener('submit', (e) => {
     element.style.display = 'none'
     element.innerHTML = ''
   }
-  inputElementArray.map((inputElement, index) => {
-    if (index !== 4) {
-      if (inputElement.value === '') {
-        textNode = 'Input Empty'
-        validate = validateElement[index]
 
-        validate.append(textNode)
-        validate.style.display = 'block'
-      } else {
-        // check text lenght
-        if (inputElement.value.length <= 4) {
-          textNode = 'Text to short'
+  inputElementArray
+    .map((inputElement, index) => {
+      if (index !== 4) {
+        if (inputElement.value === '') {
+          textNode = 'Input Empty'
           validate = validateElement[index]
 
           validate.append(textNode)
           validate.style.display = 'block'
         } else {
-          // success
-          console.log(inputElement.value, ', index ->', index)
+          // check text lenght
+          if (inputElement.value.length <= 4) {
+            textNode = 'Text to short'
+            validate = validateElement[index]
 
-          form.querySelector('#form-message').style.display = 'block'
+            validate.append(textNode)
+            validate.style.display = 'block'
+          } else {
+            checkArray.push('success')
+            // success
+            // console.log(inputElement.value, ', index ->', index)
+            // form.querySelector('#form-message').style.display = 'block'
+          }
         }
       }
-    }
-  })
+    })
+    .then(handleSubmit(e))
+  // .then(console.log(e))
 })
