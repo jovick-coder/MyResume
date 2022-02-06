a57abefe0772efeb316febeb120e498de320963d
Josiah Victor <victorjosiahm3@gmail.com>
2022-02-06 21:41:04 +0100

    another test on the form

--- assets/js/validate.js
+++ assets/js/validate.js
@@ -1,9 +1,9 @@
-// Netlify form submittion
+document.querySelector('form').addEventListener('submit', handleSubmit)
+
 const handleSubmit = (e) => {
-  // e.preventDefault();
-  // let myForm = document.getElementById("pizzaOrder");
-  // console.log('herer')
-  let formData = new FormData(e)
+  e.preventDefault()
+  let myForm = document.getElementById('MyResumeForm')
+  let formData = new FormData(myForm)
   fetch('/', {
     method: 'POST',
     headers: { 'Content-Type': 'application/x-www-form-urlencoded' },
@@ -12,57 +12,3 @@ const handleSubmit = (e) => {
     .then(() => console.log('Form successfully submitted'))
     .catch((error) => alert(error))
 }
-const form = document.querySelector('.email-form')
-form.addEventListener('submit', (e) => {
-  e.preventDefault()
-  checkArray = []
-  let inputElementArray = [...e.srcElement]
-  let validateElement = [...form.querySelectorAll('.validate')]
-
-  for (let i = 0; i < validateElement.length; i++) {
-    const element = validateElement[i]
-    element.style.display = 'none'
-    element.innerHTML = ''
-  }
-
-  inputElementArray
-    .map((inputElement, index) => {
-      if (index !== 4) {
-        if (inputElement.value === '') {
-          textNode = 'Input Empty'
-          validate = validateElement[index]
-
-          validate.append(textNode)
-          validate.style.display = 'block'
-        } else {
-          // check text lenght
-          if (inputElement.value.length <= 4) {
-            textNode = 'Text to short'
-            validate = validateElement[index]
-
-            validate.append(textNode)
-            validate.style.display = 'block'
-          } else {
-            checkArray.push('success')
-            // success
-            // console.log(inputElement.value, ', index ->', index)
-            // form.querySelector('#form-message').style.display = 'block'
-          }
-        }
-      }
-    })
-    .then(() => {
-      if (checkArray.length !== 4) {
-        let myForm = document.getElementById('MyResumeForm')
-        let formData = new FormData(myForm)
-        fetch('/', {
-          method: 'POST',
-          headers: { 'Content-Type': 'application/x-www-form-urlencoded' },
-          body: new URLSearchParams(formData).toString(),
-        })
-          .then(() => console.log('Form successfully submitted'))
-          .catch((error) => alert(error))
-      }
-    })
-  // .then(console.log(e))
-})
