6ecc05fc8bcae81b98e9c87bc49c44b11f3fd835
Josiah Victor <victorjosiahm3@gmail.com>
2022-02-06 22:16:56 +0100

    added form validation

--- assets/js/validate.js
+++ assets/js/validate.js
@@ -1,14 +1,53 @@
-const handleSubmit = (e) => {
+const form = document.querySelector('#MyResumeForm')
+form.addEventListener('submit', (e) => {
   e.preventDefault()
-  let myForm = document.getElementById('MyResumeForm')
-  let formData = new FormData(myForm)
-  fetch('/', {
-    method: 'POST',
-    headers: { 'Content-Type': 'application/x-www-form-urlencoded' },
-    body: new URLSearchParams(formData).toString(),
-  })
-    .then(() => console.log('Form successfully submitted'))
-    .catch((error) => alert(error))
-}
+  checkArray = []
+  let inputElementArray = [...e.srcElement]
+  let validateElement = [...form.querySelectorAll('.validate')]
 
-document.querySelector('form').addEventListener('submit', handleSubmit)
+  for (let i = 0; i < validateElement.length; i++) {
+    const element = validateElement[i]
+    element.style.display = 'none'
+    element.innerHTML = ''
+  }
+
+  inputElementArray
+    .map((inputElement, index) => {
+      if (index !== 4) {
+        if (inputElement.value === '') {
+          textNode = 'Input Empty'
+          validate = validateElement[index]
+
+          validate.append(textNode)
+          validate.style.display = 'block'
+        } else {
+          // check text lenght
+          if (inputElement.value.length <= 4) {
+            textNode = 'Text to short'
+            validate = validateElement[index]
+
+            validate.append(textNode)
+            validate.style.display = 'block'
+          } else {
+            checkArray.push('success')
+            // success
+            // console.log(inputElement.value, ', index ->', index)
+            // form.querySelector('#form-message').style.display = 'block'
+          }
+        }
+      }
+    })
+    .then(() => {
+      if (checkArray.length !== 4) {
+        let formData = new FormData(form)
+        fetch('/', {
+          method: 'POST',
+          headers: { 'Content-Type': 'application/x-www-form-urlencoded' },
+          body: new URLSearchParams(formData).toString(),
+        })
+          .then(() => console.log('Form successfully submitted'))
+          .catch((error) => alert(error))
+      }
+    })
+  // .then(console.log(e))
+})
